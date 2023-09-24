ad85000e70ae42aae558e0eea19c9776a77b6032
garyghayrat <gary.ghayrat@pm.me>
2023-09-24 08:46:55 -0400

    Add events and clean up

--- src/DCADaily.sol
+++ src/DCADaily.sol
@@ -3,24 +3,41 @@ pragma solidity ^0.8.13;
 
 import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
 import {IUniversalRouter} from "universal-router/interfaces/IUniversalRouter.sol";
+/**
+ * @title DCADaily
+ * @author @GaryGhayrat
+ *
+ * The system's goal is to have a Dollar-Cost Averaging system onChain,
+ * where DAI-ETH swaps happen regularly once the user `signUp`s.
+ *
+ * 1. User calls `signUp(uint256)`.
+ * 2. User gives `token` allowance to this contract.
+ * 3. Chainlink keeper regularly calls the `buy` function.
+ * 4. The `buy` function transfer in the specified amount of token,
+ * and swaps them into ETH using Uniswap's Universal Router.
+ * @notice This contract is a hackathon level code, be cautious when giving allowance to this contract.
+ */
 
 contract DCADaily {
     address[] public users;
+    address public immutable tokenAddress;
+    address public immutable WETH;
     mapping(address => bool) public isUser;
     mapping(address => uint256) public recurringBuyAmount;
-    address public tokenAddress;
-    address private immutable WETH;
-    IUniversalRouter private constant universalRouter = IUniversalRouter(0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD); // Sepolia
+    IUniversalRouter private constant universalRouter = IUniversalRouter(0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD); // Sepolia & Arbitrum
 
     // Uniswap params
     bytes private path;
     bytes private constant commands = abi.encodePacked(bytes1(uint8(0x00)), bytes1(uint8(0x0c))); // Command for V3_SWAP_EXACT_IN and UNWRAP_ETH
     bytes3 private constant LOW_FEE_TIER = bytes3(uint24(500));
 
+    // Events
+    event NewSignUp(address indexed _user, uint256 indexed _amount);
+    event UpdatedBuyAmount(address indexed _user, uint256 _updatedAmount);
+
     constructor(address _tokenAddress, address _weth) {
         tokenAddress = _tokenAddress;
         WETH = _weth;
-        // universalRouter = IUniversalRouter(_universalRouter);
         path = bytes.concat(bytes20(address(tokenAddress)), LOW_FEE_TIER, bytes20(address(WETH)));
     }
 
@@ -28,12 +45,14 @@ contract DCADaily {
         if (!isUser[msg.sender]) {
             users.push(msg.sender);
             isUser[msg.sender] = true;
+            emit NewSignUp(msg.sender, _amount);
         }
         recurringBuyAmount[msg.sender] = _amount;
+        emit UpdatedBuyAmount(msg.sender, _amount);
     }
 
     // Chainlink will call this function
-    // Right now, anyone can call this, but we should change it to only be called by Chainlink
+    // Right now, anyone can call this, but we should change it to only be called by Chainlink if possible
     function buy() external {
         for (uint256 _i = 0; _i < users.length; _i++) {
             address _user = users[_i];
@@ -46,14 +65,13 @@ contract DCADaily {
 
     function updateRecurringAmount(uint256 _amount) external {
         recurringBuyAmount[msg.sender] = _amount;
+        emit UpdatedBuyAmount(msg.sender, _amount);
     }
 
-    // TODO: Change to internal
     function _transferInTokens(address _user) private {
         IERC20(tokenAddress).transferFrom(_user, address(this), recurringBuyAmount[_user]);
     }
 
-    // TODO: change to internal
     function _swapTokensForEth(address _recepient, uint256 _tokenAmount) private {
         // // Encoding the inputs for V3_SWAP_EXACT_IN
         bytes[] memory inputs = new bytes[](2);
